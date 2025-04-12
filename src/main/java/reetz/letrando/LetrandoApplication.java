7f61242207b1d90a598b389951cfddf570c20744
JenniferReetz <jenniferheloisareetz@gmail.com>
2025-04-12 16:04:00 -0300

    Criando o projeto de teste

---
+++ src/main/java/reetz/letrando/LetrandoApplication.java
@@ -0,0 +1,13 @@
+package reetz.letrando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetrandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetrandoApplication.class, args);
+	}
+
+}
