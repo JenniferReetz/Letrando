7f6930260f40e898e90fbafe389ea2ac1c374dc1
JenniferReetz <jenniferheloisareetz@gmail.com>
2025-04-12 15:04:25 -0300

    Criando o projeto
    Adicionando ao git o projeto com as dependências do spring.

---
+++ src/test/java/reetz/letrando/LetrandoApplicationTests.java
@@ -0,0 +1,13 @@
+package reetz.letrando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetrandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
